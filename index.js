c261bc596ce542ed8e42beb7be9c8ac406a2e95c
Tandon245 <tandonabhishek245@gmail.com>
2023-09-22 22:22:40 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+
+
+// for each Loop in Js
+
+
+/* var arr=[1,2,3,4,4,5,2,1];
+arr.forEach(function(ele){
+    console.log(ele);
+}) */
+
+
+// console.error("this is an error ");
+// console.warn("this is a waring for this site");
+
+// let have block scope
+
+
+
+// Array Operations in js
+
+
+// let Arr1=["Abhi","Tandon",false,null];
+
+//shift() will remove the front element in the array
+
+/*
+Arr1.shift();
+console.log(Arr1);
+*/
+
+ 
+// push() will add a element into the array in last 
+
+/*
+Arr1.push("pushed Element");
+console.log(Arr1);
+*/
+
+
+// pop() will remove the last Element in the array
+
+/*
+
+Arr1.pop();
+console.log(Arr1);
+
+*/
+
+
+// DOM Manipulation
+
+// document.getElementById('click');
+
+//let elementClass=document.getElementsByClassName("container");
+//elementClass[0].style.background="grey";
+
+
+
+
+// Evemts In JavaScript
+
+
